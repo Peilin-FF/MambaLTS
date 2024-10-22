28aa916ce448c50279fa7d1e38e962b16fc2a634
Peilin-FF <1092854742@qq.com>
2024-10-22 21:40:57 +0800

    Update exp_long_term_forecasting.py

--- exp/exp_long_term_forecasting.py
+++ exp/exp_long_term_forecasting.py
@@ -21,7 +21,7 @@ class Exp_Long_Term_Forecast(Exp_Basic):
         
     def _build_model(self):
         model = self.model_dict[self.args.model].Model(self.args).float()
-        wandb.init(project="Mamba2_mutilayers",name=self.args.runname,entity="peilin-ff-beihang-university")
+        wandb.init(project="your own projection name",name=self.args.runname,entity="your entity")
         
         wandb.watch(models=model,criterion=nn.MSELoss,log="all",log_freq=50,log_graph=True)
         if self.args.use_multi_gpu and self.args.use_gpu:
